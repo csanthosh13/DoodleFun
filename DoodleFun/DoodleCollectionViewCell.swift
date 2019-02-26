1468a2f7fa7f2ecf8cd9d456c977cea4f1d805c4
Santhosh Chandrasekharan <csanthosh13@gmail.com>
2019-02-26 22:43:04 +0530

    Initial Commit

---
+++ DoodleFun/DoodleCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DoodleCollectionViewCell.swift
+//  DoodleFun
+//
+//  Created by Santosh Chandrasekharan on 20/02/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class DoodleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
